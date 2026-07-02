b53f99ce061f78578eea479d1394c368c80fdab6
shubhamgupta8899 <shubhamkrgupta2026@gmail.com>
2026-07-03 00:09:44 +0530

    Initial project setup with Docker and PostgreSQL

---
+++ backend/src/main/java/com/shubham/HirePilot/HirePilotApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.HirePilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirePilotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shubham/HirePilot/HirePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.HirePilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
