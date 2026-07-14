ebd4e776960a657050e184382a7a1da8b120b5f6
shubhamgupta8899 <shubhamkrgupta2026@gmail.com>
2026-07-14 21:54:00 +0530

    complete ResumeController

--- backend/src/main/java/com/shubham/HirePilot/resume/controller/ResumeController.java
+++ backend/src/main/java/com/shubham/HirePilot/resume/controller/ResumeController.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/resume")
@@ -34,5 +32,29 @@ public class ResumeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(resume);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Resume>> getUserResume(Authentication authentication){
+
+        List<Resume> resumes = resumeService.getUserResume(1L); // Hardcoded for now
+        return ResponseEntity.ok(resumes);
+    }
+
+    @GetMapping("/{resumeId}")
+    public ResponseEntity<Resume> getResume(
+            @PathVariable Long resumeId,
+            Authentication authentication){
+
+        Resume resume = resumeService.getResume(resumeId, 1L);
+        return ResponseEntity.ok(resume);
+    }
+
+    @DeleteMapping("/{resumeId}")
+    public ResponseEntity<Void> deleteResume(
+            @PathVariable Long resumeId,
+            Authentication authentication){
+
+        resumeService.deleteResume(resumeId, 1L);
+        return ResponseEntity.noContent().build();
+    }
 
 }
